19ce5afc1e4bfede9b0721b77c2cae6b6d466700
ahmed mohammed <65685434+amh092@users.noreply.github.com>
2025-06-14 02:58:42 +0300

    vercel

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import XOGame from './XOGame';
+import { Analytics } from "@vercel/analytics/next"
 
 function App() {
-  return <XOGame />;
+
+  return
+  <>
+  
+  <Analytics />
+
+<XOGame />;
+  </>
 }
 
 export default App
